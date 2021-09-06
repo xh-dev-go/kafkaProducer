6508dad858cfb32498f259b4469b13f1101c2cb3
CORPHQ-HK-PCCW\01731363 <xeth.cw.hung@pccw.com>
2021-09-06 15:58:56 +0800

    basic version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module kafkaProducer
+
+go 1.16
+
+require github.com/segmentio/kafka-go v0.4.17
---
+++ kafkaProducer.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/segmentio/kafka-go"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+var topic, server string
+
+func kafkaProducer(server string) *kafka.Writer {
+	w := &kafka.Writer{
+		Addr: kafka.TCP(server),
+		// NOTE: When Topic is not defined here, each Message must define it instead.
+		Balancer: &kafka.LeastBytes{},
+	}
+	return w
+}
+func send_message(writer *kafka.Writer, topic string, key string, msg string) {
+
+	err := writer.WriteMessages(context.Background(),
+		// NOTE: Each Message has Topic defined, otherwise an error is returned.
+		kafka.Message{
+			Topic: topic,
+			Key:   []byte(key),
+			Value: []byte(msg),
+		},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	//if err := writer.Close(); err != nil {
+	//	log.Fatal("failed to close writer:", err)
+	//}
+}
+
+func readInput(writer *kafka.Writer, topic string, doneChan chan bool, delay int64, shouldPrint bool) {
+	reader := bufio.NewReader(os.Stdin)
+	var index = 0
+
+	var delayFor time.Duration
+	var hasDelay = false
+	if delay > 0 {
+		hasDelay = true
+		delayFor = time.Duration(delay) * time.Millisecond
+	}
+
+	for {
+		if hasDelay {
+			time.Sleep(delayFor)
+		}
+		index++
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			doneChan <- true
+			break
+		}
+
+		if shouldPrint {
+			println(string(line))
+		}
+		send_message(writer, topic, fmt.Sprintf("N_key_%06d", index), string(line))
+		//println(line)
+	}
+}
+
+func readAllInput(writer *kafka.Writer, topic string, doneChan chan bool, shouldPrint bool) {
+	reader := bufio.NewReader(os.Stdin)
+	var res = ""
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		res = res + "\n" + string(line)
+	}
+	if shouldPrint {
+		println(res)
+	}
+	send_message(writer, topic, fmt.Sprintf("N_key_%s", time.Now().Format(time.RFC3339)), res)
+	doneChan <- true
+}
+
+func main() {
+	var oneOff bool
+	var shouldPrint bool
+	var delay int64
+	flag.StringVar(&topic, "topic", "", "kafka topic")
+	flag.StringVar(&server, "host", "", "kafka host")
+	flag.BoolVar(&oneOff, "single-message", false, "toggle on if read all the input and send once")
+	flag.BoolVar(&shouldPrint, "print", false, "toggle on if want to print the message transferred")
+	flag.Int64Var(&delay, "delay", -1, "delay for ms, only work if not in single-message mode")
+	flag.Parse()
+
+	if len(os.Args) == 1 {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if topic == "" {
+		panic(errors.New("topic is empty"))
+	}
+	if server == "" {
+		panic(errors.New("server is empty"))
+	}
+
+	writer := kafkaProducer(server)
+
+	var doneChan = make(chan bool)
+	if oneOff {
+		go readAllInput(writer, topic, doneChan, shouldPrint)
+	} else {
+		go readInput(writer, topic, doneChan, delay, shouldPrint)
+	}
+
+	for {
+		select {
+		case isDone := <-doneChan:
+			if isDone {
+				os.Exit(0)
+			}
+		}
+	}
+}
